e7217973e42be61bfcc951f093bfe3e16f2e5125
Ubuntu <ubuntu@profone.us-central1-a.c.orbital-nuance-301915.internal>
2021-03-22 18:49:36 +0000

    book and user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserController } from './user/user.controller';
+import { UserService } from './user/user.service';
+import { BookController } from './book/book.controller';
+import { BookService } from './book/book.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, UserController, BookController],
+  providers: [AppService, UserService, BookService],
 })
 export class AppModule {}
